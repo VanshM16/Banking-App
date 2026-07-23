eaa65f38b7ca95a677e7d55bed5a52cc02079f12
VanshM16 <vanshmittal483@gmail.com>
2026-07-23 19:49:33 +0530

    Implement homepage UI with charts and sidebar components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,27 @@
+import HeaderBox from '@/components/ui/HeaderBox'
+import TotalBalanceBox from '@/components/ui/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
+  const loggedIn={ firstname:"Vansh"};
   return (
-    <div>Home</div>
+    <section className="home">
+      <div className='home-content'>
+        <header className='home-header'>
+            <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstname||'guest'}
+            subtext="Access and manage your account and transactions efficiently."
+            />
+            <TotalBalanceBox 
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+            />
+        </header>
+      </div>
+      </section>
   )
 }
 
