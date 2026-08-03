5b1acc0d3dae9837cd2af256a4d2a0509904f65b
VanshM16 <vanshmittal483@gmail.com>
2026-08-03 10:47:33 +0530

    feat: add authentication form and custom input components
    - Implemented AuthForm component for user sign-in and sign-up
    - Created CustomInput component for form fields
    - Added form handling using react-hook-form and zod for validation
    - Introduced UI components for form structure including Form, FormField, FormLabel, FormControl, FormMessage
    - Added Card component for layout consistency
    - Integrated user actions for sign-in and sign-up functionality
    - Updated package.json with new dependencies for form handling and UI components

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,16 @@
+'use server'
+
+export const signIn = async () => {
+    try{
+        //Mutation/Database/Fetch
+    } catch(error){
+        console.log('Error', error);
+    }
+}
+export const signUp = async () => {
+    try{
+        //Mutation/Database/Fetch
+    } catch(error){
+        console.log('Error', error);
+    }
+}
